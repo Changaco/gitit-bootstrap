63e252b42d5ce576592385e5abaaa7bbbb5b5a4e
Changaco <changaco@changaco.oy.lc>
2014-03-05 13:43:05 +0100

    don't fail when npm is not installed

--- build.sh
+++ build.sh
@@ -6,7 +6,14 @@ cat src/js/*.js >static/js/gitit.js
 cat src/css/*.css >static/css/gitit.css
 
 minify="$projectroot/node_modules/minify/bin/minify"
-[ ! -f $minify ] && npm i minify
+if [ ! -f $minify ]; then
+    if which npm &>/dev/null; then
+        npm i minify
+    else
+        echo "npm not found, styles and scripts won't be minified";
+        minify="cp"
+    fi
+fi
 
 cd static/js
 $minify gitit.js gitit.min.js || exit 1
